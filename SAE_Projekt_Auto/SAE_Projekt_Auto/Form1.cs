48149112dd548e2d3ffc6002985920a8fe581cf2
Baaaatz <154369931+Baaaatz@users.noreply.github.com>
2023-12-20 21:16:04 +0100

    Add files via upload

--- SAE_Projekt_Auto/SAE_Projekt_Auto/Form1.cs
+++ SAE_Projekt_Auto/SAE_Projekt_Auto/Form1.cs
@@ -12,7 +12,7 @@ namespace SAE_Projekt_Auto
         //Marvin
 
         //Daniel
-        string startupPath = Environment.CurrentDirectory;
+        string startupPath = Environment.CurrentDirectory;                                                   //gibt den Pfad zurück, in dem die ausführte Datei liegt
         string dateiName = "AutoDatenbank.csv";
         string dateiPfad;
 
@@ -22,31 +22,26 @@ namespace SAE_Projekt_Auto
             laden();
         }
 
-         
-
         public void laden()
         {
             
-            dateiPfad = Path.Combine(startupPath, dateiName);            //gibt den Pfad zurück, in dem die ausführbare Datei Anwendung liegt
-
+            dateiPfad = Path.Combine(startupPath, dateiName);                                                
 
             if (!File.Exists(dateiPfad))
             {
-
-                using (StreamWriter writer = File.CreateText(dateiPfad))                 // Erstellen der Datei, falls sie nicht existiert
+                using (StreamWriter writer = File.CreateText(dateiPfad))                                     // Erstellen der Datei, falls sie nicht existiert
                 {
 
-                    writer.WriteLine("Marke,Modell,Kilometerstand,Baujahr,Preis");      // Header der CSV Datei
+                    writer.WriteLine("Marke,Modell,Kilometerstand,Baujahr,Preis");                           // Header der CSV Datei
 
                 }
 
-                rtbAusgabe.AppendText($"Die Datei '{dateiName}' wurde im Pfad '{startupPath}' erstellt."); //Ausgabe in Textbox
+                rtbAusgabe.AppendText($"Die Datei '{dateiName}' wurde im Pfad '{startupPath}' erstellt.");   //Ausgabe in Textbox
             }
             else
             {
                 rtbAusgabe.AppendText($"Die Datei '{dateiName}' existiert bereits im Pfad '{startupPath}'."); //Ausgabe in Textbox
             }
-
         }
 
 
@@ -58,7 +53,7 @@ namespace SAE_Projekt_Auto
         private void btSpeichern_Click(object sender, EventArgs e)
         {
 
-            if (String.IsNullOrWhiteSpace(tbMarke.Text) ||              //if Abfrage ob Textboxen leer sind
+            if (String.IsNullOrWhiteSpace(tbMarke.Text) ||                                                     //if Abfrage ob Textboxen leer sind
                 String.IsNullOrWhiteSpace(tbModell.Text) ||
                 String.IsNullOrWhiteSpace(tbKilometerstand.Text) ||
                 String.IsNullOrWhiteSpace(tbBaujahr.Text) ||
@@ -74,7 +69,7 @@ namespace SAE_Projekt_Auto
                 string baujahr = tbBaujahr.Text;
                 string preis = tbPreis.Text;
 
-                Wagen newAuto = new Wagen
+                Wagen newAuto = new Wagen                                                                     //Erstellt neues Objekt nach Wagen typ in Klasse AutolisteSpeichern
                 {
                     Marke = marke,
                     Modell = modell,
@@ -85,9 +80,9 @@ namespace SAE_Projekt_Auto
                 AutolisteSpeichern.Add(newAuto);
 
                 SchreibeInCSV(AutolisteSpeichern);
-                AutolisteSpeichern.RemoveAt(AutolisteSpeichern.Count - 1);
+                AutolisteSpeichern.RemoveAt(AutolisteSpeichern.Count - 1);                                     //löscht Objekt aus AutolisteSpeichern
 
-                tbMarke.Clear();
+                tbMarke.Clear();                                                                              //Cleart Textboxen
                 tbModell.Clear();
                 tbKilometerstand.Clear();
                 tbBaujahr.Clear();
@@ -100,19 +95,16 @@ namespace SAE_Projekt_Auto
             rtbAusgabe.Clear();
 
             dateiPfad = Path.Combine(startupPath, dateiName);
-
-            // StreamWriter verwenden, um die CSV-Datei zu erstellen oder zu öffnen
-            using (StreamWriter sw = new StreamWriter(dateiPfad,true))
-            {       
-
-                // Daten jeder Zeile schreiben
-                foreach (Wagen auto in autolisteSpeichern)
+         
+            using (StreamWriter sw = new StreamWriter(dateiPfad,true))                                             // StreamWriter aufruf zur CSV-Datei
+            {                           
+                foreach (Wagen auto in autolisteSpeichern)                                                        //Durchläuft jedes Objekt der liste AutolisteSpeichern
                 {
-                    sw.WriteLine($"{auto.Marke},{auto.Modell},{auto.Kilometerstand},{auto.Baujahr},{auto.Preis}");
+                    sw.WriteLine($"{auto.Marke},{auto.Modell},{auto.Kilometerstand},{auto.Baujahr},{auto.Preis}"); //Autoinfos werden in CSV-Datei geschrieben
                 }
             }
 
-            rtbAusgabe.AppendText($"Daten wurden in die CSV-Datei geschrieben: '{dateiPfad}' ");
+            rtbAusgabe.AppendText($"Daten wurden in die CSV-Datei geschrieben: '{dateiPfad}' ");                 //Ausgabe der Statusbox
         }
 
                 //Daniel
@@ -121,21 +113,21 @@ namespace SAE_Projekt_Auto
         private void btSuchen_Click(object sender, EventArgs e)
         {
 
-            LadeAutoInformationen();                                // Methode zum Lese der CSV-Datei und erstellt Wagen-Objekte
+            LadeAutoInformationen();                                                                         // Methode zum Lese der CSV-Datei 
 
-            string marke = tbMarke.Text;                                    // Eingegebene Daten aus den Textboxen
-            string modell = tbModell.Text;
+            string marke = tbMarke.Text;                                                                     // Eingegebene Daten aus den Textboxen
+            string modell = tbModell.Text;  
             string kilometerstand = tbKilometerstand.Text;
             string baujahr = tbBaujahr.Text;
             string preis = tbPreis.Text;
          
-            var uebereinstimmungen = AutolisteSuchen.Where(autosuche =>             // Vergleiche die eingegebenen Daten mit den Wagen-Objekten aus der Liste
+            var uebereinstimmungen = AutolisteSuchen.Where(autosuche =>                                      // Filter der eingegebenen Daten mit den Objekten aus der LadeAutoInformationen Liste
             {
                 if (!string.IsNullOrWhiteSpace(marke) && !autosuche.Marke.Equals(marke, StringComparison.OrdinalIgnoreCase))
                     return false;
 
                 if (!string.IsNullOrWhiteSpace(modell) && !autosuche.Modell.Equals(modell, StringComparison.OrdinalIgnoreCase))                  //Filtert Raus ob textboxen leer oder beschrieben sind, Ignoriert leere textboxen
-                    return false;
+                    return false;                                                                                                                //ignoriert case sensitive
 
                 if (!string.IsNullOrWhiteSpace(kilometerstand) && !autosuche.Kilometerstand.Equals(kilometerstand, StringComparison.OrdinalIgnoreCase))
                     return false;
@@ -149,12 +141,11 @@ namespace SAE_Projekt_Auto
                 return true; 
             }
             ).ToList();
-
-            rtbHauptausgabe.Text = "";                                          // Gib die Übereinstimmungen in die Hauptausgabe-TextBox aus
+                
+            rtbHauptausgabe.Text = "";                                                                                                               // Gib die Übereinstimmungen in der Hauptausgabe aus
             foreach (var wagen in uebereinstimmungen)
             {
-                rtbHauptausgabe.AppendText($"Marke: {wagen.Marke}, Modell: {wagen.Modell}, Kilometerstand: {wagen.Kilometerstand}km, Baujahr: {wagen.Baujahr}, Preis: {wagen.Preis}€\n");
-                
+                rtbHauptausgabe.AppendText($"Marke: {wagen.Marke}, Modell: {wagen.Modell}, Kilometerstand: {wagen.Kilometerstand}km, Baujahr: {wagen.Baujahr}, Preis: {wagen.Preis}€\n");             
             }
 
             rtbAusgabe.Clear();
@@ -163,20 +154,20 @@ namespace SAE_Projekt_Auto
 
         private void LadeAutoInformationen()
         {
-            string dateiPfad = "AutoDatenbank.csv";
+            dateiPfad = Path.Combine(startupPath, dateiName);
 
             try
             {
-                AutolisteSuchen.Clear();                                                 // Leere die Liste, um alte Daten zu entfernen
+                AutolisteSuchen.Clear();                                                              // Leere die Liste, um alte Daten zu entfernen
                
-                using (StreamReader reader = new StreamReader(dateiPfad))                   // Lese die CSV-Datei Zeile für Zeile
+                using (StreamReader reader = new StreamReader(dateiPfad))                             // Lese die CSV-Datei Zeile für Zeile
                 {
                     string line;
                     while ((line = reader.ReadLine()) != null)
                     {
-                        string[] parts = line.Split(',');                           // Trennt ,
+                        string[] parts = line.Split(',');                                             // Trennt ,
                     
-                        Wagen wagen = new Wagen                                     // Erstelle ein neues Wagen-Objekt und fülle es mit den Daten aus der CSV-Datei
+                        Wagen wagen = new Wagen                                                       // Erstellt ein neues Objekt und füllt es mit den Daten der CSV-Datei
                         {
                             Marke = parts[0],
                             Modell = parts[1],
@@ -184,14 +175,13 @@ namespace SAE_Projekt_Auto
                             Baujahr = parts[3],
                             Preis = parts[4]
                         };
-
-                        AutolisteSuchen.Add(wagen);                             // Füge das Wagen-Objekt zur Liste hinzu
+                        AutolisteSuchen.Add(wagen);                                                    // Fügt das Objekt der Liste hinzu
                     }
                 }
             }
             catch (Exception ex)
             {
-                rtbAusgabe.AppendText($"Fehler beim Laden der Autoinformationen: {ex.Message}");
+                rtbAusgabe.AppendText($"Fehler beim Laden der Autoinformationen: {ex.Message}");    //Statusbox ausgabe
             }
         }
     }                //Abdul
